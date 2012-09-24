7486bc59aeeb25533a245d6908b50878437ce639
Milan Marković <zivotinja@gmail.com>
2012-09-24 10:41:13 +0200

    Fixed Trie, added values

--- Trie.hs
+++ Trie.hs
@@ -1,33 +1,74 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 import Data.Function
+import GHC.Generics
+import Data.Serialize
+import Control.Applicative
+import Data.Char
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
 
-data Trie a = End
-          | Trie [(a, Trie a)]
-          deriving (Show)
+import Data.List (foldl')
+import Codec.Compression.GZip
 
-data Pair a b = Pair a b
-               deriving (Show)
-
-instance (Eq a) => Eq (Pair a b) where
-  (Pair x _) == (Pair y _) = x == y
-
-insert :: (Eq a) => [a] -> Trie a -> Trie a
-insert [] trie  = trie
-insert (x:xs) End = Trie$ [(x, (insert xs End))]
-insert (x:xs) (Trie []) = Trie$ [(x,(insert xs End))]
-insert (x:xs) (Trie ((y,tr):ys))
-  | x == y = Trie$ (y,(insert xs tr)):ys
-  | otherwise = Trie$ (x, (insert xs End)):(y,tr):ys
-
-find :: (Eq a) => [a] -> Trie a -> Maybe [a]
-find []   _          = Just []
-find _    End        = Nothing
-find _    (Trie [])  = Nothing
-find (x:xs) t@(Trie ((y,tr):ys))
-  | x == y =
-    case find xs tr of
-      Nothing -> Nothing
-      Just z  -> Just (x:z)
-  | otherwise = find (x:xs) (Trie ys)
+data Trie a b = Tip  (Maybe b)
+              | Trie (Maybe b) [(a, Trie a b)]
+              deriving (Show, Generic, Eq)
+                       
+instance (Serialize a, Serialize b) => Serialize (Trie a b)
 
 
-a = insert "help" $ insert "hello" End
+insert :: (Eq a) => [a] -> b -> Trie a b -> Trie a b
+insert [] v (Tip _)     = Tip  (Just v)
+insert [] v (Trie _ l)  = Trie (Just v) l
+insert (x:xs) v1 (Tip  v2)    = Trie v2 $ [(x, (insert xs v1 (Tip Nothing)))]
+insert (x:xs) v1 (Trie v2 []) = Trie v2 $ [(x, (insert xs v1 (Tip Nothing)))]
+insert (x:xs) v1 (Trie v2 ((y,tr):ys))
+  | x == y =    Trie v2$ (y,(insert xs v1 tr)):ys
+  | null ys   = Trie v2$ (x,(insert xs v1 (Tip Nothing))):(y,tr):[]
+  | otherwise = (Trie v2 [(y,tr)]) `join` (insert (x:xs) v1 (Trie v2 ys))
+   where
+     join (Trie v1 [x]) (Trie _ ys) = Trie v1 (x:ys)
+     join (Trie v1 xs) (Trie _ ys)  = Trie v1 (xs ++ ys)
+
+find ::  (Eq a) => [a] -> Trie a b -> Maybe b
+find []  (Tip  v)       = v
+find xs  (Tip  v)       = Nothing
+find []  (Trie v _)     = v
+find xs  (Trie v [])    = Nothing
+find (x:xs) (Trie v ((y,tr):ys))
+  | x == y    = find xs tr 
+  | otherwise = find (x:xs) (Trie v ys)
+
+
+-- Examples
+
+type WordCount = (Int, Trie Char Int)
+
+emptyWordC = (1::Int, Tip Nothing)
+insertWordC (n,trie) word = 
+  case find word trie of
+    Nothing -> (n+1, insert word n trie)
+    Just x  -> (n,trie)
+wordToInt (n,trie) word = find word trie
+
+type WordFreq = Trie Char Int
+
+emptyWordF = Tip Nothing
+insertWordF trie word = 
+  case find word trie of
+    Just n -> insert word (n+1) trie
+    Nothing -> insert word 1 trie
+
+enc x  = compress $ BL.fromChunks [encode x]
+dec x  = decode $ BS.concat $ BL.toChunks (decompress x)
+
+main = do 
+  wrds <- (words . map toLower) <$> readFile "/usr/share/dict/words"
+  let dict = foldl' insertWordC emptyWordC wrds
+  BL.writeFile "./words.dict.gz" (enc dict)
+
+readDict :: IO (Either String WordCount)
+readDict = do
+  bytes <- BL.readFile "./words.dict.gz"
+  return (dec bytes)
